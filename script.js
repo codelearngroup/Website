c4da872734b7cfc3c0adceaa2d774ad0e802a8a6
CoderTeam1 <72059049+CoderTeam1@users.noreply.github.com>
2021-04-17 12:25:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-
+function add() {
+  var numone = document.getElementById("btn01");
+  var numtwo = document.getElementById("btn02");
+  var num1parsed = parseInt(numone);
+  var num2parsed = parseInt(numtwo);
+}
